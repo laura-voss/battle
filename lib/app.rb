798ab70bb41d3e832d937c4551ecdc580527a5e3
Laura Voss <lauravoss@posteo.de>
2022-05-03 16:13:27 +0100

    adding sinatra/reloader

--- lib/app.rb
+++ lib/app.rb
@@ -1,1 +1,18 @@
 require 'sinatra'
+require "sinatra/reloader" if development?
+
+get '/' do
+  "hello!"
+end
+
+get '/secret' do
+  "secret"
+end
+
+get '/info' do
+  "special stuff"
+end
+
+get '/more' do
+  "more more more"
+end
