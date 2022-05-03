8f7e3c1cd377e8c054c8e7bd4a9e6d89d8047c62
StevenSpiegl <steven.spiegl@gmail.com>
2022-05-03 17:43:55 +0100

    refactored view logic to controller logic

--- lib/app.rb
+++ lib/app.rb
@@ -2,5 +2,7 @@ require 'sinatra'
 require "sinatra/reloader" if development?
 
 get '/cat' do
+  @name = ["Amigo", "Misty", "Almond"].sample
   erb(:index)
+  #order is important... switch these around and it doesn't work
 end
