1ee5192ded7a01a4e527c22f6d86ad11e7dc9f08
StevenSpiegl <steven.spiegl@gmail.com>
2022-05-03 16:50:50 +0100

    cats without borders

--- lib/app.rb
+++ lib/app.rb
@@ -16,3 +16,26 @@ end
 get '/more' do
   "more more more"
 end
+
+get '/cat' do
+  #Their way:
+
+  "<div style='border: 10px dashed red'>
+    <img src='https://i.imgur.com/jFaSxym.png'>
+  </div>"
+
+  #Our way:
+
+  # "<style>
+  #   img {border: 10px dashed red}
+  # </style>
+  # <div>
+  #   <img src='https://i.imgur.com/jFaSxym.png'>
+  # </div>"
+
+  #A bit of both:
+
+  # "<div>
+  #   <img style='border: 10px dashed red' src='https://i.imgur.com/jFaSxym.png'>
+  # </div>"
+end
