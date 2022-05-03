2eb480e381c814064751a487c671c11143e6e44e
Laura Voss <lauravoss@posteo.de>
2022-05-03 18:12:29 +0100

    using params

--- lib/app.rb
+++ lib/app.rb
@@ -1,8 +1,16 @@
 require 'sinatra'
 require "sinatra/reloader" if development?
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Misty", "Almond"].sample
+  erb(:index)
+  #order is important... switch these around and it doesn't work
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  @age = params[:age]
   erb(:index)
   #order is important... switch these around and it doesn't work
 end
