d351bb758c7d89fa3b162292b22be7a35533124a
sjtinsley <simonjtinsley@gmail.com>
2022-05-04 16:34:29 +0100

    POST method for cat name

--- lib/app.rb
+++ lib/app.rb
@@ -4,13 +4,18 @@ require "sinatra/reloader" if development?
 get '/random-cat' do
   @name = ["Amigo", "Misty", "Almond"].sample
   erb(:index)
-  #order is important... switch these around and it doesn't work
 end
 
-get '/named-cat' do
+post '/named-cat' do
   p params
   @name = params[:name]
   @age = params[:age]
   erb(:index)
-  #order is important... switch these around and it doesn't work
+end
+
+get '/cat-form' do
+  p params
+  @name = params[:name]
+  @age = params[:age]
+  erb(:cat_form)
 end
